930c510c58a011bd505557bb70b197fc815fa8fa
beyamor <beyamor@gmail.com>
2013-06-11 21:47:26 -0700

    Initial commit

---
+++ src/poplar/Main.hx
@@ -0,0 +1,31 @@
+package poplar;
+
+import com.haxepunk.Engine;
+import com.haxepunk.HXP;
+import flash.display.StageAlign;
+import flash.display.StageScaleMode;
+import flash.Lib;
+
+/**
+ * ...
+ * @author beyamor
+ */
+
+class Main extends Engine
+{
+	public function new() {
+		
+		super(640, 640);
+	}
+	
+	override public function init():Dynamic 
+	{
+		super.init();
+		HXP.console.enable();
+	}
+	
+	public static function main() {
+		
+		new Main();
+	}
+}
