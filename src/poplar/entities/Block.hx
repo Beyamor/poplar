a73274b2bd85f841d5036d6c538404150ce31de0
beyamor <beyamor@gmail.com>
2013-06-12 19:47:26 -0700

    Coloring blocks

--- src/poplar/entities/Block.hx
+++ src/poplar/entities/Block.hx
@@ -6,15 +6,27 @@ import nme.display.BitmapData;
 /**
  * ...
  * @author beyamor
- */
+ */ 
 class Block extends Entity
 {
+	private static var colors:Array<Int> = [0xE01B1B, 0x3485F7, 0x32ED11, 0xF5952F, 0xFCED12, 0xF285D2];
+	private static var colorIndex:Int = 0;
+	private static function nextColor():Int {
+		var color = colors[colorIndex % colors.length];
+		++colorIndex;
+		return color;
+	}
+	
 	public static var	WIDTH:Int	= 48;
 	public static var	HEIGHT:Int	= 48;
 	
 	public function new(x:Float, y:Float) 
-	{
-		super(x, y, new Image("img/block.png"));
+	{		
+		var sprite = new Image("img/block.png");
+		sprite.color = nextColor();
+		
+		super(x, y, sprite);
+		
 	}
 	
 }
